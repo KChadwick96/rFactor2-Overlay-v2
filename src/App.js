63a73bd7bf30b0ca0ffcd903efbe4b5225a127f5
Kieran Chadwick <kieranchadwick@rentalcars.com>
2020-01-10 22:05:58 +0000

    StandingRow work

--- src/App.js
+++ src/App.js
@@ -7,12 +7,33 @@ const App = () => {
   return (
     <Fragment>
       <div style={{ position: "absolute", bottom: "12px", left: "12px" }}>
-        <DriverTag position={1} name="Kieran Chadwick" team="KRT" />
+        <DriverTag position={1} name="Kieran Chadwick" team="A racing team" />
       </div>
 
-      <div style={{ width: "280px", padding: "12px" }}>
-        <StandingRow position={1} driverName="Chadwick" driverNameCode="CHA" />
-        <StandingRow position={2} driverName="Carpenter" driverNameCode="CAR" />
+      <div style={{ width: "260px", padding: "12px" }}>
+        <StandingRow
+          position={1}
+          driverName="Chadwick"
+          driverNameCode="CHA"
+          teamColour="red"
+          gapToLeader={0}
+        />
+        <StandingRow
+          position={2}
+          driverName="Carpenter"
+          driverNameCode="CAR"
+          teamColour="turquoise"
+          gapToLeader={1.2}
+          interval={1.2}
+        />
+        <StandingRow
+          position={12}
+          driverName="Mangan"
+          driverNameCode="MAN"
+          teamColour="yellow"
+          gapToLeader={4.3}
+          interval={3.1}
+        />
       </div>
     </Fragment>
   );
--- src/components/StandingRow/StandingRow.js
+++ src/components/StandingRow/StandingRow.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import "./StandingRow.scss";
 
 const VARIANTS = {
-  LEADER_GAP: "leader-gap",
   INTERVAL_GAP: "interval-gap",
+  LEADER_GAP: "leader-gap",
   POSITION: "position"
 };
 
-const StandingRow = ({ position, driverName, driverNameCode, variant }) => {
+const TyreGraphic = () => {
+  return <div className="tyre-graphic">M</div>;
+};
+
+const StandingRow = ({
+  position,
+  driverName,
+  driverNameCode,
+  teamColour,
+  gapToLeader,
+  interval,
+  variant = VARIANTS.INTERVAL_GAP
+}) => {
+  const name = variant === VARIANTS.POSITION ? driverName : driverNameCode;
+
+  const gap = useMemo(() => {
+    let value = 0;
+
+    if (variant === VARIANTS.INTERVAL_GAP) {
+      if (position === 1) return "Interval";
+      value = interval;
+    }
+
+    if (variant === VARIANTS.LEADER_GAP) {
+      if (position === 1) return "Leader";
+      value = gapToLeader;
+    }
+
+    return `+${value.toFixed(3)}`;
+  }, [position, variant, interval, gapToLeader]);
+
   return (
     <li className="standing-row">
-      <div className="standing-row__position">{position}</div>
-      <div className="standing-row__name">{driverName}</div>
-      <div className="standing-row__interval">+0.467</div>
+      <div
+        className="standing-row__position"
+        style={{ borderRightColor: teamColour }}
+      >
+        {position}
+      </div>
+      <TyreGraphic />
+      <div className="standing-row__name">{name}</div>
+      <div className="standing-row__interval">{gap}</div>
     </li>
   );
 };
