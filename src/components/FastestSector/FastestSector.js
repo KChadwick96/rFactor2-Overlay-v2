ce279eb2bf752ceb5a3dc71eef8d9ea8cde3b47d
Craig-97 <eoschazmr@googlemail.com>
2020-03-13 22:59:20 +0000

    update classNames for fastestSector

--- src/components/FastestSector/FastestSector.js
+++ src/components/FastestSector/FastestSector.js
@@ -2,9 +2,10 @@ import React from "react";
 
 import "./FastestSector.scss";
 
-const FastestSector = ({ sector, sectorTime, driverName }) => {
+const FastestSector = ({ sector, sectorTime, driverName, className }) => {
+  const classNames = useMemo(() => ["fastest-sector", className].join(" "), [className]);
   return (
-    <div className="fastest-sector">
+    <div className={classNames}>
       <div className="fastest-sector__number">Sector {sector}</div>
       <div className="fastest-sector__name">{driverName}</div>
       <div className="fastest-sector__time">{sectorTime}</div>
