23ebdfd57d81b53228e18514242b076df5f2a8ae
Craig-97 <eoschazmr@googlemail.com>
2020-03-24 13:47:19 +0000

    tidy up

--- src/components/FastestSector/FastestSector.js
+++ src/components/FastestSector/FastestSector.js
@@ -10,6 +10,12 @@ const DEFAULT_SPRING = {
   stiffness: 30
 };
 
+const HEADER_SPRING = {
+  type: "spring",
+  damping: 20,
+  stiffness: 100
+};
+
 const fadeIn = {
   initial: { opacity: 0, width: 200 },
   animate: { opacity: 1 }
@@ -21,10 +27,10 @@ const FastestSector = ({ sector, sectorTime, driverName, className }) => {
   return (
     <motion.div initial={{ y: -550, x: 1688 }} style={{ width: 200 }} className={classNames}>
       <motion.div
+        className="fastest-sector__header"
         initial={{ width: 0 }}
         animate={{ width: 216 }}
-        transition={{ type: "spring", damping: 20, stiffness: 100 }}
-        className="fastest-sector__header"
+        transition={{ ...HEADER_SPRING }}
       >
         <motion.div className="fastest-sector__header-text" initial={{ width: 200 }}>
           <motion.span initial={{ x: 0, opacity: 0 }} animate={{ x: 50, opacity: 1 }} transition={{ duration: 0.5 }}>
@@ -37,6 +43,7 @@ const FastestSector = ({ sector, sectorTime, driverName, className }) => {
       </motion.div>
 
       <motion.div
+        className="fastest-sector__details"
         initial={{ y: -80, height: 0, backgroundColor: styleVars.colorPurple, opacity: 0 }}
         animate={{ height: 104, backgroundColor: styleVars.colorPrimaryDark, opacity: 1 }}
         transition={{
@@ -47,7 +54,6 @@ const FastestSector = ({ sector, sectorTime, driverName, className }) => {
             delay: 1
           }
         }}
-        className="fastest-sector__details"
       >
         <motion.div {...fadeIn} className="fastest-sector__number">
           Sector {sector}
